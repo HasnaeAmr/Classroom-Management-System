c37795ed34f928c3e0327e8e34f625867590da9a
Sara-Akharraz <saraakharraz22@gmail.com>
2024-11-22 00:55:50 +0100

    update (Erreur de frappe)

--- Gestion_de_Salle_EJB/ejbModule/metier/SalleRemote.java
+++ Gestion_de_Salle_EJB/ejbModule/metier/SalleRemote.java
@@ -1,17 +1,7 @@
 package metier;
 
-import java.util.List;
 
-import javax.ejb.Remote;
 
-import metier.entities.Role;
-import metier.entities.User;
-
-@Remote
 public interface SalleRemote {
-	public User AddUser(User user);
-	public User getUser(int id);
-	public List<User> listUser();
-	public void setUser(int id,String nom,String mdp,Role role);
-	public void removeUser(int id);
+	
 }
