da50ff525d6269db59ddf38bf88a1e7c73132045
Hasnae Amr <150762808+HasnaeAmr@users.noreply.github.com>
2024-11-23 03:06:06 +0100

    Update JournalisationEJBImp.java

--- Gestion_de_Salle_EJB/ejbModule/metier/JournalisationEJBImp.java
+++ Gestion_de_Salle_EJB/ejbModule/metier/JournalisationEJBImp.java
@@ -1,15 +1,23 @@
 package metier;
 
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import metier.entities.Journalisation;
 
 public class JournalisationEJBImp implements JournalisationLocal, JournalisationRemote{
 
 	@PersistenceContext(unitName = "SallesEJB")
-    	private EntityManager em;
+    private EntityManager em;
+	
+	public List<Journalisation> listJournalisation(){
+		Query req = em.createQuery("select j from Journalisation j");
+        return req.getResultList();
+	}
 	
 	public void journaliser(String description) {
 		Journalisation j = new Journalisation(description);
