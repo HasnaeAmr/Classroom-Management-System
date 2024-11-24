707c00084cee6ffd1ef9e7a8181285e08e4693bc
Hasna Amarmach <amarmachhasna22@gmail.com>
2024-11-24 01:30:09 +0100

    Merge: adding frontend features

--- Gestion_de_Salle_EJB/ejbModule/metier/CategorieLocal.java
+++ Gestion_de_Salle_EJB/ejbModule/metier/CategorieLocal.java
@@ -11,9 +11,7 @@ public interface CategorieLocal {
 	
 	public Categorie getCategorie(int id_categorie);
 	public List<Categorie> listCategorie();
-<<<<<<< HEAD
 	public void setCategorie(int id_categorie,String type_categorie);
-=======
->>>>>>> d3934161c145de4dacb56a9c8c17b1aef201af01
+
 	public void removeCategorie(int id_categorie);
 }
