342f3b713ef77ab5744ffb3fb43ada8cffc9a158
Hasnae Amr <150762808+HasnaeAmr@users.noreply.github.com>
2024-11-25 22:49:54 +0100

    Ajout des méthodes pour les états des salles

--- Gestion_de_Salle_EJB/ejbModule/metier/SalleRemote.java
+++ Gestion_de_Salle_EJB/ejbModule/metier/SalleRemote.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ejb.Remote;
 
+import metier.entities.EtatSalle;
 import metier.entities.Horaire;
 import metier.entities.Jour;
 import metier.entities.Salle;
@@ -14,11 +15,17 @@ public interface SalleRemote {
 	public Salle ajouterSalle(Salle s);
 	public void modifierSalle(Long id, Salle s);
 	public void supprimerSalle(Long id);
-	public List<Salle> filtreJH(Horaire h, Jour j);
-	public List<Salle> filtreJ(Jour j);
-	public List<Salle> filtreH(Horaire h);
-	public List<Salle> filtreP(Long id);
+	public List<EtatSalle> filtreJH(Horaire h, Jour j);
+
+	public EtatSalle filtreEtatNom(String nom);
+	public List<EtatSalle> filtreJ(Jour j);
+	public List<EtatSalle> filtreH(Horaire h);
+	public List<EtatSalle> filtreP(int id);
+	public List<EtatSalle> filtrePJH(Long id, Horaire h, Jour j);
+	public List<EtatSalle> filtrePJ(Long id, Jour j);
+	public List<EtatSalle> filtrePH(Long id, Horaire h);
 	public boolean getEtat(Long id, Horaire h, Jour j);
 	public void setEtat(Long id, Horaire h, Jour j , boolean e);
-	public List<Salle> getSallesVides();
+	public List<EtatSalle> getSallesVides();
+	public List<EtatSalle> getEtatSalles();
 }
