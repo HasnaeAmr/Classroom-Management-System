dd09a9abf75d4ffadf406699e19bb810c0cea515
Hasnae Amr <150762808+HasnaeAmr@users.noreply.github.com>
2025-01-28 02:02:09 +0100

    Add functions for the classroom EJB (Remote)

--- Gestion_de_Salle_EJB/ejbModule/metier/SalleRemote.java
+++ Gestion_de_Salle_EJB/ejbModule/metier/SalleRemote.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import javax.ejb.Remote;
 
+import metier.entities.Categorie;
 import metier.entities.EtatSalle;
 import metier.entities.Horaire;
 import metier.entities.Jour;
+import metier.entities.MatiereFiliereCategorie;
 import metier.entities.Salle;
 import metier.entities.User;
 @Remote
@@ -15,6 +17,7 @@ public interface SalleRemote {
 	public Salle getSalle(Long id);
 	public Salle ajouterSalle(Salle s);
 	public void modifierSalle(Long id, Salle s);
+	public List<Salle> getSallesByCategorie(int id_categorie);
 	public void supprimerSalle(Long id);
 	public List<EtatSalle> filtreJH(Horaire h, Jour j);
 	public EtatSalle getEtatById(Long id);
@@ -27,7 +30,10 @@ public interface SalleRemote {
 	public List<EtatSalle> filtrePJ(Long id, Jour j);
 	public List<EtatSalle> filtrePH(Long id, Horaire h);
 	public boolean getEtat(Long id, Horaire h, Jour j);
-	public void setEtat(Long id, Horaire h, Jour j , boolean e, User prof);
+	public void setEtat(Long id, boolean e, MatiereFiliereCategorie mat);
 	public List<EtatSalle> getSallesVides();
 	public List<EtatSalle> getEtatSalles();
+	public void setEtat(Long id_salle, Horaire horaire, Jour jour, boolean e);
+	public List<EtatSalle> getSallesVidesByHoraireNDJourAndCat(Categorie cat, Horaire h, Jour j);
+	public List<EtatSalle> getLesSallesVidesByCategorie(Categorie cat);
 }
